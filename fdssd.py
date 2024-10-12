90eea9743dcc38d6b9e45c2e724c03f2a651a470
ming777-ss <2291141007@qq.com>
2024-10-12 21:19:57 +0800

    Update fdssd.py

--- fdssd.py
+++ fdssd.py
@@ -17,41 +17,5 @@ st.text('''
 举头望明月，低头思故乡。
 ''')
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 
-# 字符串
-st.write("这是一段文本。")
-
-# 数字
-st.write(42)
-
-# 列表
-st.write([1, 2, 3])
-
-# 字典
-st.write({"key": "value"})
-
-# 数据框（DataFrame）
-df = pd.DataFrame({"Column 1": [1, 2, 3], "Column 2": ["A", "B", "C"]})
-st.write(df)
-
-# 多参数用法
-st.write("这是一个字符串", 42, [1, 2, 3], {"key": "value"})
-
-# 自定义渲染
-fig, ax = plt.subplots()
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
-ax.plot(x, y)
-st.write(fig)
-
-data = {
-    'latitude': [37.7749, 34.0522, 40.7128],
-    'longitude': [-122.4194, -118.2437, -74.0060],
-    'name': ['San Francisco', 'Los Angeles', 'New York']
-}
-
-st.map(data, zoom=4, use_container_width=True)
 # streamlit run fdssd.py
